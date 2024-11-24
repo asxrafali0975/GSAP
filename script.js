3d0b5d6c010a768ec6e832253d246067374f71a9
AshrafAli0975 <2023bca113@axiscolleges.in>
2024-11-24 19:52:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
- gsap.from("div",{
-    x:1000,
-    duration:2,
-    rotate:380,
-    backgroundColor:"blue",
-    borderRadius:"50%",
-    opacity:0,
-    stagger:1
-})
 
-gsap.to("h1",{
-    x:1000,
-    duration:3,
-   stagger:0.4,  // can be -1 
-    repeat:Infinity,
-    yoyo:true
+gsap.from(".box1", {
+    rotate: 360,
+    x: 700,
+    delay: 1,
+    duration: 2
+});
 
-})
+gsap.from(".page2 .box2", {  //page2 ke andar ka box
+    rotate: 360,
+    x: 700,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".page2 .box2", 
+        scroller:"body",
+        markers:"true",
+        start:"top 50%",
+        end:"top 70%",
+        scrub:5
+       
+    
+    }
+});
+
+gsap.from(".box3", {
+    rotate: 360,
+    x: 700,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".box3",
+       
+    }
+});
